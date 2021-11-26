116ba6843ff0cbfde7de5c71b2f58d4469015f6d
shunSchool <72032942+shunSchool@users.noreply.github.com>
2021-11-26 11:32:08 -0700

    snow works

--- main.js
+++ main.js
@@ -5,19 +5,17 @@ cnv.height = 800
 
 //empty array of snowflakes
 let snowflakes = [];
-
 //main draw loop
 requestAnimationFrame(draw);
 function draw() {
     // fill canvas
-    background("black");
+     background("black");
 
     //move the snowflakes
     for (let i = 0; i < snowflakes.length; i++) {
         moveSnowflake (snowflakes[i]);
-        drawSnowflake(snowflake[i]);
+        drawSnowflake(snowflakes[i]);
     }
-
     //loop draw function
     requestAnimationFrame(draw);
 }
@@ -26,6 +24,38 @@ function draw() {
 function newRandomSnowflake() {
     return {
         x: randomInt(0, cnv.width),
-        y: randomInt(0, )
+        y: 0,
+        r: randomInt(2, 5),
+        color: "white"
+    }
+}
+
+function drawSnowflake (aSnowflake) {
+    fill ("white")
+    circle(aSnowflake.x, aSnowflake.y, aSnowflake.r, "fill")
+}
+
+function moveSnowflake(aSnowflake) {
+    // aSnowflake.x += randomInt(1, 3);
+    aSnowflake.y += randomInt(2, 5)
+    if (aSnowflake.y > cnv.height) {
+        aSnowflake.y = 0
+    }
+}
+
+function addSnowflake() {
+    snowflakes.push(newRandomSnowflake());
+}
+
+//events 
+document.addEventListener("keydown", keydownHandler);
+
+function keydownHandler(event) {
+    if (event.keyCode === 39) { //right arrow
+        //add new random snowflake
+       addSnowflake()
+    } else if (event.keyCode = 37) { //left arrow
+        //remove last snowflake
+        snowflakes.pop
     }
 }
